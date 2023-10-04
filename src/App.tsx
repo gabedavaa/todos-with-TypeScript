e0b7dad7ba83e19287f5614d92176dc82d0ec981
gabedavaa <110985736+gabedavaa@users.noreply.github.com>
2023-10-04 21:44:45 +0400

    adding files

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
-import React from "react";
+import { useState } from "react";
 import "./App.css";
+import NewTodo from "./component/NewTodo";
+import Todos from "./component/Todos";
+import Todo from "./models/todos";
 
 function App() {
-  return <div className="App"></div>;
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
+
+    setTodos((prev) => prev.concat(newTodo));
+  };
+
+  return (
+    <div className="App">
+      <NewTodo onAddTodo={addTodoHandler} />
+      <Todos items={todos} />
+    </div>
+  );
 }
 
 export default App;
--- src/component/Todos.tsx
+++ src/component/Todos.tsx
@@ -1,5 +1,15 @@
-const Todos = () => {
-  return <div></div>;
+import React from "react";
+import Todo from "../models/todos";
+import TodoItem from "./TodoItem";
+
+const Todos: React.FC<{ items: Todo[] }> = (props) => {
+  return (
+    <ul>
+      {props.items.map((item) => (
+        <TodoItem key={item.id} text={item.text} />
+      ))}
+    </ul>
+  );
 };
 
 export default Todos;
